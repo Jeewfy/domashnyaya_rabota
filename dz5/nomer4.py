d1f034ee5cd9db003cbbd8cc65ff566cc2a9e704
Jeewfy <166714146+Jeewfy@users.noreply.github.com>
2024-11-08 17:16:40 +0000

    Сделал 4 задание

---
+++ dz5/nomer4.py
@@ -0,0 +1,9 @@
+number1 = int(input("Введите первое чиcло: "))
+number2 = int(input("Введите второе число: "))
+if number1 == number2:
+    print("Числа равны")
+elif number1 < number2:
+    print(number1,number2)
+else:
+    print(number2, number1)
+
