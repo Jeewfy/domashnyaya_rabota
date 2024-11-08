8d9794386631fd1f7411a5015218ebf1a72937fe
Jeewfy <166714146+Jeewfy@users.noreply.github.com>
2024-11-08 17:28:38 +0000

    сделал задачу номер 3

--- dz6/nomer1.py
+++ dz6/nomer1.py
@@ -1,11 +1,11 @@
 number = int(input("Введите число в диапазоне от 1 до 100: "))
-if number % 3 == 0 and number % 5 ==0:
+if number < 1 or number > 100:
+    print("Это не число от 1 до 100")
+elif number % 3 == 0 and number % 5 ==0:
     print("Fizz Buzz")
 elif number % 5 == 0:
     print("Buzz")
 elif number % 3 == 0:
     print("Fizz")
-elif number % 3 != 0 and number % 5 != 0:
-    print(number)
 else:
-    print("Это не число от 1 до 100")
+    print(number)
