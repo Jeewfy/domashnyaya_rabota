dbd2464395a74cae2d9abdf4329dd3445702560c
Jeewfy <166714146+Jeewfy@users.noreply.github.com>
2024-11-08 16:57:44 +0000

    Сделал задание номер 3

---
+++ dz5/nomer3.py
@@ -0,0 +1,9 @@
+number = int(input("Введите число: "))
+if number > 0:
+    print("Numberis positive")
+if number < 0:
+    print("Number is negative")
+if number == 0:
+    print("Number is equal to zero")
+
+        
