96b4834e454f2f0da770ec25e948b52728e5c044
Jeewfy <166714146+Jeewfy@users.noreply.github.com>
2024-11-15 18:29:16 +0000

    Блок2 задание 6

--- classwork/task6.py
+++ classwork/task6.py
@@ -1,5 +1,5 @@
-sec  = int(input("Введите колличество секунд: "))
+sec = int(input("Введите колличество секунд: "))
 hours = sec // 3600
 minutes = (sec - hours * 3600) // 60
 seconds = (sec - ((hours * 3600)+(minutes * 60)))
-print(hours, minutes,seconds)
+print(hours, minutes, seconds)
