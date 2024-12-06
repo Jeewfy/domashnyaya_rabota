52d7a26624ba6d9d3d67b33d7eb9da7772a064ff
Misha <mikekapikan@yandex.ru>
2024-12-06 19:51:21 +0300

    Сделал 1 задачу

--- dz10/nomer1.py
+++ dz10/nomer1.py
@@ -1,4 +1,4 @@
-num1 = int(input("Введите первое число: "))
+'''num1 = int(input("Введите первое число: "))
 num2 = int(input("Введите второе число: "))
 list_of_number =[]
 
@@ -9,4 +9,23 @@ for i in range(num1, num2 + 1):
         for j in range(2, i):
             if i % j != 0:
                 list_of_number.append(i)
-print(set(list_of_number))
+print(set(list_of_number))'''
+
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i ==0:
+            return False
+    return True
+
+def print_primes_in_range(start, end):
+    print(f"Простые числа в диапазоне от {start} до {end}: ")
+    for num in range(start, end + 1):
+        if is_prime(num):
+            print(num, end=' ')
+    print()
+
+start_range = int(input("Введите начальное число диапазона: "))
+end_range = int(input("Введите конченое число диапазона: "))
+print_primes_in_range(start_range,end_range)
