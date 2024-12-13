ab76baca0266d8538e9957191b9e4b757bbdd91b
Jeewfy <mikekapikan@yandex.ru>
2024-12-13 20:45:27 +0300

    Задача номер3

--- classwork/sbornik_zadach/zadacha3.py
+++ classwork/sbornik_zadach/zadacha3.py
@@ -1,7 +1,8 @@
 number = int(input("Введите трехзначное число: "))
-desyatki = number // 10
+desyatki = (number // 10) % 10
 edinichki = number % 10
-suma = desyatki + edinichki
-proizvedenie = desyatki * edinichki
+sotie = number // 100
+suma = desyatki + edinichki + sotie
+proizvedenie = desyatki * edinichki * sotie
 print(f"в числе {number}, \n десятки - {desyatki},\n",
       f"единицы - {edinichki},\n suma - {suma}, \n произведение - {proizvedenie}")
